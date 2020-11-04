2e5a30dee3c2b5cbeb54bdcdb2d2d7c4b754ce43
EasonHuang <44485603+EasonHuang-tw@users.noreply.github.com>
2020-11-04 21:19:13 +0800

    add test file for pi

---
+++ HW2/part1/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo ###### test1 #######
+time ./pi.out 1 100000000
+
+echo ###### test2 #######
+time ./pi.out 2 100000000
+
+echo ###### test3 #######
+time ./pi.out 3 100000000
+
+echo ###### test4 #######
+time ./pi.out 4 100000000
+
+echo ###### test5 #######
+time ./pi.out 5 100000000
+
+echo ###### test6 #######
+time ./pi.out 6 100000000
+
+echo ###### test7 #######
+time ./pi.out 7 100000000
+
+echo ###### test8 #######
+time ./pi.out 8 100000000
+
+echo ###### test9 #######
+time ./pi.out 9 100000000
