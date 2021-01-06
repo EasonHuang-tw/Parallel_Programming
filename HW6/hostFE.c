e4ddbdcacf0ef2c80b0c3f31ccf5f5ebdb6cef98
EasonHuang-tw <eason27271563.me05@nctu.edu.tw>
2021-01-06 09:08:49 +0000

    [HW6] first commit

--- HW6/hostFE.c
+++ HW6/hostFE.c
@@ -3,11 +3,104 @@
 #include "hostFE.h"
 #include "helper.h"
 
+
 void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
             float *inputImage, float *outputImage, cl_device_id *device,
             cl_context *context, cl_program *program)
 {
-    cl_int status;
-    int filterSize = filterWidth * filterWidth;
 
+//	cl memory	
+    cl_int status;
+	cl_kernel kernel;
+    cl_command_queue queue;
+
+	cl_mem input_buffer;
+	cl_mem output_buffer;
+	cl_mem filter_buffer;
+
+	
+	const size_t global = imageHeight * imageWidth;
+	const size_t local = 1000;
+
+	int filterSize = filterWidth * filterWidth;
+	int i , j;
+	/*	kernal	*/
+	
+	kernel = clCreateKernel(*program, "convolution", &status);
+	
+	queue = clCreateCommandQueue(*context, *device, NULL, &status);
+
+	/*	create buffers	*/	
+
+// input
+	input_buffer = clCreateBuffer(	*context,CL_MEM_READ_ONLY,
+									sizeof(cl_float) * imageHeight * imageWidth,
+									NULL, 
+									&status);
+// filter
+	filter_buffer = clCreateBuffer(	*context,CL_MEM_READ_ONLY,
+									sizeof(cl_float) * filterSize ,
+									NULL , 
+									&status);
+// output
+	output_buffer = clCreateBuffer(	*context,CL_MEM_WRITE_ONLY,
+									sizeof(cl_float) * imageHeight * imageWidth,
+									NULL , 
+									&status);
+	/*	write buffers*/
+	 clEnqueueWriteBuffer ( queue , input_buffer, CL_TRUE, 0 , sizeof(cl_float) * imageHeight * imageWidth, (void *)inputImage, 0, NULL, NULL);
+	//if(err){printf("%d, writebuffer Error",err);}
+	 clEnqueueWriteBuffer ( queue , filter_buffer, CL_TRUE, 0 , sizeof(cl_float) * filterSize,  (void *)filter, 0, NULL, NULL);
+
+	
+	clSetKernelArg(kernel, 0, sizeof(int), &filterWidth);
+	clSetKernelArg(kernel, 1, sizeof(cl_mem), &filter_buffer);
+	clSetKernelArg(kernel, 2, sizeof(int), &imageHeight);
+	clSetKernelArg(kernel, 3, sizeof(int), &imageWidth);
+	clSetKernelArg(kernel, 4, sizeof(cl_mem), &input_buffer);
+	clSetKernelArg(kernel, 5, sizeof(cl_mem), &output_buffer);
+/*
+   if (err != CL_SUCCESS)
+    {
+        printf("Error: Failed to set kernel arguments! %d\n", err);
+        exit(1);
+    }
+*/
+	/* execute */
+	clEnqueueNDRangeKernel(queue, kernel, 1, NULL, &global , &local, 0, NULL, NULL);
+/*
+	if (err != CL_SUCCESS)
+    {
+        printf("Error: Failed to execute kernel!,err num = %d\n",err);
+//        return EXIT_FAILURE;
+    }
+*/
+	/*	load the result		*/
+	clEnqueueReadBuffer(queue, output_buffer, CL_TRUE, 0, sizeof(cl_float) * imageHeight * imageWidth, outputImage, 0, NULL, NULL);
+/*
+	if(err){
+		printf("NO!!!!");
+	}
+*/
+/*
+    clReleaseMemObject(input_buffer);
+    clReleaseMemObject(output_buffer);
+    clReleaseMemObject(filter_buffer);
+    clReleaseKernel(kernel);
+    clReleaseCommandQueue(queue);
+*/
+/*
+	for( i = 0 ; i<imageHeight ;i++){
+		for( j = 0 ; j<imageWidth ; j++ ){
+			outputImage[i*imageWidth+j] = 20.0f;
+		}
+	}
+
+	for( i = 0 ; i<imageHeight ;i++){
+		for( j = 0 ; j<imageWidth ; j++ ){
+			printf("idx = %d , num = %.1f \n", i*imageWidth+j,outputImage[i*imageWidth+j]);
+		}
+		
+	}
+*/
 }
--- HW6/kernel.cl
+++ HW6/kernel.cl
@@ -1,4 +1,31 @@
-__kernel void convolution() 
-{
-   
+__kernel void convolution(	
+							const int filterWidth,
+							__constant float* filter,
+							const int imageHeight,
+							const int imageWidth,
+							__global float* inputImage,
+							__global float* outputImage
+						){	
+
+	const int i = get_global_id(0);
+	const int j = get_global_id(1);
+	const int halfWidth = filterWidth/2;
+	float sum = 0;
+	int k =0;
+	int l = 0;
+	const int idxTmp = j * get_global_size(0) +i;
+	const int x = idxTmp % imageWidth;
+	const int y = idxTmp / imageWidth;
+
+	for( k = -halfWidth ; k <= halfWidth ; k++  ){
+		for ( l = -halfWidth ; l <= halfWidth ; l++  ){
+			if( y + k >= 0 && y + k < imageHeight && x + l >=0 && x + l < imageWidth){
+				sum  += inputImage[(idxTmp + k*imageWidth + l)] * filter[(k+halfWidth)*filterWidth + l + halfWidth];
+			}
+
+		}
+	}
+
+	outputImage[idxTmp] = sum;
+
 }
